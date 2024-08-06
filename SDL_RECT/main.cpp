c8045a64fcb8b51b07d5b94b51a7495c7fae2d80
someone <someone@some.where>
2024-08-06 17:21:09 +0300

    one firebal is working

--- SDL_RECT/main.cpp
+++ SDL_RECT/main.cpp
@@ -1,16 +1,17 @@
 #include <SDL2/SDL.h>
 #include "rect.hpp"
+#include <vector>
 
 int main(int, char* []) {
     SDL_Window* window = SDL_CreateWindow("Jeu de la vie", SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
+        1280,
+        960,
         SDL_WINDOW_SHOWN);
 
     // Setup renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+    std::vector<Rect> fireballs;
     // Set render color to red
     SDL_SetRenderDrawColor(renderer, 25, 0, 0, 255);
 
@@ -18,20 +19,40 @@ int main(int, char* []) {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     Rect r;
-    r.position.x = 10;
-    r.position.y = 20;
-    r.position.h = 30;
-    r.position.w = 40;
+    r.position.x = 640;
+    r.position.y = 960;
+    r.position.h = 20;
+    r.position.w = 20;
 
-    // Draw rect
+    fireballs.push_back(r);
+
     r.draw_r(renderer, r.position);
+    for (int i = 0; i < 100; i++) {
+        r.draw_r(renderer, r.position);
+        SDL_SetRenderDrawColor(renderer, 25, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 25, 255, 255);
+        r.position.x += r.x_speed;
+        r.position.y -= r.y_speed;
+        r.y_speed -= 3;
+        SDL_Delay(100);
 
-    bool keep_window_open = true;
-    while (keep_window_open) {
-        while (SDL_PollEvent()
+
+
+
+        SDL_Event e;
+        while (SDL_PollEvent(&e) > 0)
         {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                break;
+            }
 
+            SDL_UpdateWindowSurface(window);
         }
+        
     }
     return 0;
+
 }
